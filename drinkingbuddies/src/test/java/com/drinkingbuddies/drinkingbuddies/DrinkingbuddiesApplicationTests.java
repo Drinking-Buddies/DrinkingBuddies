fe6053b6090b47ff8fc51b23be166ee06dc3e3fc
David Xu <davidx@usc.edu>
2022-04-25 16:33:29 -0700

    Added project

---
+++ drinkingbuddies/src/test/java/com/drinkingbuddies/drinkingbuddies/DrinkingbuddiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinkingbuddies.drinkingbuddies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkingbuddiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
